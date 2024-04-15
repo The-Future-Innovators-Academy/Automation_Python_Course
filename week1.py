7f1ec9c8e5ca57fad5e6b9931e4cbcba2e573b03
Satyadev Moolagani <smoolagani@gmail.com>
2024-04-15 18:16:55 -0400

    Finished Week 1

--- week1.py
+++ week1.py
@@ -58,15 +58,61 @@ print(product)
 print(sum1)
 print(cubed)
 
+clear_screen()
 
 # Comparision Operators
+num1 = 5
+num2 = 5
+
+less_than = num1 < num2
+print(less_than)
+
+greater_than = num1 > num2
+print(greater_than)
+
+equal_to = num1 == num2
+print(equal_to)
+
+clear_screen()
 
 
 # Logical Operators
+num1 = 1
+num2 = 21
+num3 = 69
 
+compare1 = num1 < num2 and num2 < num3
+print(compare1)
 
+clear_screen()
 # If Statements
+num1 = 87
+num2 = 420
 
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
 
 # Converting Weights Exercise
+# in to ft, ft to miles, m to km, cm to m, g to kg
+    
+while True:
+    user_input = input("What do you want to convert (q to quit): ")
+    if user_input == "q":
+        break
+    
+
+    if user_input == "in":
+        inches = int(input("How many inches do you want to convert to feet: "))
+        print(inches / 12)
+    elif user_input == "ft":
+        feet = int(input("How many feet do you want to convert to miles: "))
+        print(feet / 5280)
+    elif user_input == "m":
+        meters = int(input("How many meters do you want to convert to km: "))
+        print(meters / 1000)
+    else:
+        print("Incorrect Input")
+
 
